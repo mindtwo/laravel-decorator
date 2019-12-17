2ab319500b6a5a032120f2e3defaf390eb281c65
Tobi Kivelip <tkivelip@gmail.com>
2019-12-17 17:23:23 +0100

    Example test

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace mindtwo\LaravelDecorator\Tests\Feature;
+
+use mindtwo\LaravelAutoCreateUuid\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->asserTrue(true);
+    }
+}
